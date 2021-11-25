f762f52256646881b7e9ab5cdce0ca6a35f7ae88
baileywickham <baileywickham@gmail.com>
2021-11-25 09:52:47 -1000

    Update lang model, add python notebook, add datafilter

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Characterize and generate pretty cities
 
 [Street view API](https://developers.google.com/maps/documentation/streetview/overview)
+
+## Use
+Requries `pipenv` and `python3.9`. Use `pipenv install` to install dependencies.
+
+Use `filterdata.py <filename>` to filter a set of data from the spreadsheet.
--- gMaps.py
+++ gMaps.py
@@ -1,75 +1,92 @@
 import requests
-import sys
 import re
 import secretm
-import csv
-from typing import Union
 import argparse
+from typing import Union, Optional
 
+import random
+import string
 
 
 class GMaps:
     url: str = 'https://maps.googleapis.com/maps/api/streetview'
 
-    def __init__(self, api_key : str, signature : str = None,
-            size='180x180', pano=None ):
+    def __init__(self, api_key : str, signature:str=None,
+            size='180x180', pano:str=None ):
         self.api_key = api_key
         self.signature = signature
         self.size = size
-        self.pano : str = pano
+        self.pano : Optional[str] = pano
 
     def dmsToDec(self, coord: str) -> float:
         deg, minutes, seconds, direction =  re.split('''[°'"]''', coord)
         return (float(deg) + float(minutes)/60 + float(seconds)/(60*60)) * (-1 if direction in ['W', 'S'] else 1)
 
     def isDMS(self, maybeDMS: Union[str, float]) -> bool:
-        if type(maybeDMS) == str:
+        if isinstance(maybeDMS, str):
             if "'" in maybeDMS or '"' in maybeDMS or '°' in maybeDMS:
                 return True
         return False
 
-    def getDMS(self, lat:str, long:str):
-        lat : float = self.dmsToDec(lat)
-        long : float = self.dmsToDec(long)
+    def get(self, params:dict[str,Union[str,float,None]]) -> requests.Response:
+        params['key'] = self.api_key
+        params['signature'] = self.signature
+        params['source'] = 'outdoor'
+        return requests.get(self.url, params=params)
+
+    def getDMS(self, _lat:str, _long:str):
+        lat : float = self.dmsToDec(_lat)
+        long : float = self.dmsToDec(_long)
         return self.getDec(lat, long)
 
-    def getDec(self, lat:str, long:str) -> requests.Response:
-        params : dict[str, str] = {"size": self.size,
-                'key' : self.api_key,
-                'signature' : self.signature,
-                'location': f'{lat},{long}'}
-        r = requests.get(self.url, params=params)
-        return r
+    def getDec(self, lat:Union[str, float], long:Union[str, float]) -> requests.Response:
+        params : dict[str, Union[str, None, float]] = {
+                'size': self.size,
+                'location': f'{lat},{long}',}
+        return self.get(params)
 
-    def getAndWriteToFile(self, lat:str, long:str):
+    def getAndWriteToFile(self, lat:str, long:str, filename:str):
         # assume lat and long are same format
-        filename = '{lat[0:4]}-{long[0-4]}.jpg'
+        #filename = '{lat[0:4]}-{long[0-4]}.jpg'
         if self.isDMS(lat):
             rsp = self.getDMS(lat, long)
         else:
             rsp = self.getDec(lat, long)
         if rsp.status_code != 200:
             print(f'error calling on {lat},{long}: {rsp.content}')
-            return
-        self.writeImageToFile(filename, rsp.content)
-
-
+        else:
+            self.writeImageToFile(filename, rsp.content)
 
     def writeImageToFile(self, filename: str, data):
+        print(f'writing {filename}')
         with open(filename, 'wb') as f:
             f.write(data)
 
+    def getAndVarryHeading(self, lat:str, long:str):
+        params = {'lat':lat, 'long':long}
+        # type checker is wrong here, need to cast
+        return [self.get(p) for p in [params | {'heading':x} for x in [None, 0, 90, 190, 270]]]
 
-def parseFile(filename: str, gm: GMaps):
+def runFromFile(filename: str, gm: GMaps):
     with open(filename, 'r') as f:
         for line in f:
+            score = 5
             lat, long = line.strip().split()
-            gm.getAndWriteToFile(lat, long)
+            num = ''.join(random.choice(string.digits) for i in range(4))
+            fn = f'data/{score}-{filename}-{num}.jpg'
+            gm.getAndWriteToFile(lat, long, fn)
 
 def runWithFile(filename:str):
     sm = secretm.Secrets()
     gm = GMaps(sm['api_key'])
-    parseFile(filename, gm)
+    runFromFile(filename, gm)
+
+def runWithCoord(lat:str, long:str):
+    sm = secretm.Secrets()
+    gm = GMaps(sm['api_key'])
+    filename = '{lat[0:3]}-{long[0:3]}.jpg'
+    gm.getAndWriteToFile(lat, long, filename)
+
 
 #lat, long = '''52°23'23.85"N   4°53'8.03"E'''.split()
 if __name__ == "__main__":
@@ -80,4 +97,6 @@ if __name__ == "__main__":
     args = p.parse_args()
     if args.file:
         runWithFile(args.file)
+    if args.lat and args.long:
+        runWithCoord(args.lat, args.long)
 
